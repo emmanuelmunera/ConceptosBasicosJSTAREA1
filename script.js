418a513b024ad55ea412d42faf6007e68aadf4bf
Emmanuel <foreroemmanuel8@gmail.com>
2023-10-12 11:52:22 -0500

    TAREA 1

---
+++ script.js
@@ -0,0 +1,21 @@
+var num1 = 5;
+var num2 = 7;
+
+var suma = num1 + num2;
+
+var resta = num1 - num2;
+
+var multiplicacion = num1 * num2;
+
+var division = num1 / num2;
+
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("multiplicacion:", multiplicacion);
+console.log("division:", division);
+
+alert(`El resultado de las ecuaciones es `); 
+alert(`la suma de ${num1} + ${num2} es ${suma}`);
+alert(`la resta de  ${num1} - ${num2} es ${resta}`);
+alert(`la multiplicacion de  ${num1} * ${num2} es ${multiplicacion}`);
+alert(`la division de  ${num1} / ${num2} es ${division}`);
